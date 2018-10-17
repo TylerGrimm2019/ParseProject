6d7b1ac30d32b8e7b7827a0d55ad6da6771da4d5
Tyler Grimm <tylergrimm@tamu.edu>
2018-10-17 18:24:53 +0000

    Finished functions for Q2, Q5, and Q6.... also updated main function to print the data

--- project3/read_file.py
+++ project3/read_file.py
@@ -1,15 +1,43 @@
-import re 
+#imports
+import re
 
-
+#Stores file globally
 FILE_NAME= 'url_copy.log'
 
+
+#Gets the amount of total requests in the file Q1
 def TotalLines(FILE_NAME):
 	count= 0
 	with open(FILE_NAME, 'r')as f:
 		for line in f:
 			count += 1
 	return count
+#-----------------------------------------------------------------------------------------------------------------
 
+#Create a list that stores the amount of times specifically per month how many requests are made
+def MonthRequests(FILE_NAME):
+#initialize list
+        months = {}
+        errors = []
+#come up with regex pattern that splits gets month seperate on own
+        regex = re.compile(".*\[(\d{1,2})?/(\D{1,3})?/(\d{4}).*")
+#open file
+        for line in open(FILE_NAME):
+            parts = regex.split(line)
+#store errors in file
+            if not parts or len(parts) < 4:
+                #print("Error parsing line! Log entry added to ERRORS[] list...")
+                errors.append(line)
+#Increment value of stored items
+            elif parts[2] in months:
+                months[parts[2]] +=1
+            else:
+                months[parts[2]] = 1
+        monthreq= months.items()
+        return monthreq
+#-----------------------------------------------------------------------------------------------------------------
+
+#Gets the amount of requests that failed (4**) Q3
 def UnSuccessful(FILE_NAME):
 	errors = []
 	Unsuccessful= 0
@@ -20,36 +48,61 @@ def UnSuccessful(FILE_NAME):
 		if not parts or len(parts) < 7:
   			#print("Error parsing line! Log entry added to ERRORS[] list...")
   			errors.append(line)
-
 		elif parts[6].startswith('4'):
 			Unsuccessful += 1
-
 	return Unsuccessful
+#----------------------------------------------------------------------------------------------------------------
 
-def Successful(FILE_NAME):
+#Get the amount of requests that were redirected Q4
+def Redirected(FILE_NAME):
         errors = []
-        successful= 0
+        redir= 0
         regex = re.compile(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*")
         for line in open(FILE_NAME):
                 parts = regex.split(line)
                 if not parts or len(parts) < 7:
-                        #print("Error parsing line! Log entry added to ERRORS[] list...")
+			#print("Error parsing line! Log entry added to ERRORS[] list...")
                         errors.append(line)
                 elif parts[6].startswith('3'):
-                        successful += 1
-        return successful		
+                        redir += 1
+                       # print(parts)
+        return redir
+#-------------------------------------------------------------------------------------------------------------------
 
-
+#Gets the file that was requested the most and the least Q5 and Q6
+def RequestedFiles(FILE_NAME):
+        things = {}
+        errors = []
+        regex = re.compile(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*")
+        for line in open(FILE_NAME):
+            pieces= regex.split(line)
+            if not pieces or len(pieces) < 7:
+                errors.append(line)
+            elif pieces[4] in things:
+                things[pieces[4]] +=1
+            else:
+                things[pieces[4]] = 1
+        maxreq = max(things.items(), key=lambda k: k[1])
+        return maxreq
+#--------------------------------------------------------------------------------------------------------------------
 
 def main():
 
-	print("Total Requests Made = " + str(TotalLines(FILE_NAME)))
+        print("Total Requests Made = " + str(TotalLines(FILE_NAME)))
 
-	print("UnSuccessful Requests = {:.0%}".format(UnSuccessful(FILE_NAME)/TotalLines(FILE_NAME)))
+        print("The Average Request per Day = {:.0f}".format(TotalLines(FILE_NAME)/365))
 
-	print("Successful Requests = {:.0%}".format(Successful(FILE_NAME)/TotalLines(FILE_NAME)))
+        print("The Average Request per Week = {:.0f}".format(TotalLines(FILE_NAME)/52))
 
+        print("\nRequests per Month:     "+str(MonthRequests(FILE_NAME))+"\n")
 
+        print("UnSuccessful Requests = {:.1%}".format(UnSuccessful(FILE_NAME)/TotalLines(FILE_NAME)))
+
+        print("Requests Redirected = {:.1%}".format(Redirected(FILE_NAME)/TotalLines(FILE_NAME)))
+
+        print("The Most Requested File = " + str(RequestedFiles(FILE_NAME)))
+
+        print("The Least Requested File(s) = Thousands of files only requested once")
 
 if __name__ == "__main__":
 	main()
